3f8a61140e99513d3433a2f48a4f6811463e2871
tillklockmann <till.klockmann@gmail.com>
2020-11-28 13:07:48 +0100

    add fn setUp to rm multiple class instantiations

--- tests/FizzbuzzTest.php
+++ tests/FizzbuzzTest.php
@@ -7,21 +7,29 @@ use Statista\Fizzbuzz;
 
 class FizzbuzzTest extends TestCase
 {
+    /**
+     * @var Fizzbuzz
+     */
+    private $fzzbzz;
+
+    protected function setUp() : void
+    {
+        $this->fzzbzz =  new Fizzbuzz;
+    }    
+
     /** @test */
     public function it_can_set_numbers()
     {
-        $fzzbzz = new Fizzbuzz;
-        $fzzbzz->setNumbers(1,5);
-        $this->assertSame([1,2,3,4,5], $fzzbzz->getNumbers() );
+        $this->fzzbzz->setNumbers(1,5);
+        $this->assertSame([1,2,3,4,5], $this->fzzbzz->getNumbers() );
     }
 
     /** @test */
     public function it_can_print_numbers()
     {
-        $fzzbzz = new Fizzbuzz;
-        $fzzbzz->setNumbers(1,2);
+        $this->fzzbzz->setNumbers(1,2);
         ob_start();
-        $fzzbzz->printNumbers();
+        $this->fzzbzz->printNumbers();
         $output = ob_get_contents();
         ob_end_clean();
         $this->assertMatchesRegularExpression('/1\n2\n/', $output);
@@ -30,33 +38,30 @@ class FizzbuzzTest extends TestCase
     /** @test */
     public function it_can_fizz_numbers_that_are_multiples_of_three()
     {
-        $fzzbzz = new Fizzbuzz;
-        $fzzbzz->setNumbers(1,3);
+        $this->fzzbzz->setNumbers(1,3);
         
-        $numbers = $fzzbzz->getNumbers();
-        $fuzzNumber = $fzzbzz->fuzzNumber($numbers[2]);
+        $numbers = $this->fzzbzz->getNumbers();
+        $fuzzNumber = $this->fzzbzz->fuzzNumber($numbers[2]);
         $this->assertEquals("Fizz", $fuzzNumber);
     }
 
     /** @test */
     public function it_can_buzz_numbers_that_are_multiples_of_five()
     {
-        $fzzbzz = new Fizzbuzz;
-        $fzzbzz->setNumbers(1,5);
+        $this->fzzbzz->setNumbers(1,5);
         
-        $numbers = $fzzbzz->getNumbers();
-        $fuzzNumber = $fzzbzz->fuzzNumber($numbers[4]);
+        $numbers = $this->fzzbzz->getNumbers();
+        $fuzzNumber = $this->fzzbzz->fuzzNumber($numbers[4]);
         $this->assertEquals("Buzz", $fuzzNumber);
     }
 
     /** @test */
     public function it_can_fizzbuzz_numbers_that_are_multiples_of_five_and_three()
     {
-        $fzzbzz = new Fizzbuzz;
-        $fzzbzz->setNumbers(11,15);
+        $this->fzzbzz->setNumbers(11,15);
         
-        $numbers = $fzzbzz->getNumbers();
-        $fuzzNumber = $fzzbzz->fuzzNumber($numbers[4]);
+        $numbers = $this->fzzbzz->getNumbers();
+        $fuzzNumber = $this->fzzbzz->fuzzNumber($numbers[4]);
         $this->assertEquals("FizzBuzz", $fuzzNumber);
     }
 }
